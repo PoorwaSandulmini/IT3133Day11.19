0a40db6a0d0a923bca5d01c137b691bbf541352e
PoorwaSandulmini <139644198+PoorwaSandulmini@users.noreply.github.com>
2024-11-23 19:53:53 +0000

    Modifications

---
+++ day11.19/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {useState} from "react";
+import Childcom from './Childcom';
+
+function App() {
+  const[childname,setChildname]=useState("Default");
+  const receiveName=(name)=>{
+    setChildname(name);
+  }
+
+  return(
+    <div className="App">
+      <h2>Hello React I'm Parent</h2>
+      <Childcom setfun={receiveName}/>
+      <p>Parent:My child said:{childname}</p>
+    </div>
+  );
+}
+
+export default App;
